b24c427528c1d63a71cc1fcb4a10dcb4c03be4e1
Abhishek Tiwari <tiwariabhishek27611@gmail.com>
2024-03-11 14:20:17 +0530

    typescript-part-1

---
+++ only-js.js
@@ -0,0 +1,33 @@
+// const button = document.getElementById('add')
+// const num1 = document.getElementById('num1')
+// const num2 = document.getElementById('num2')
+
+// function add(num1,num2) {
+//     return num1 + num2
+// }
+
+// button.addEventListener('click',()=>{
+//     console.log("click...", add(num1.value,num2.value));
+// })
+
+//if we pass the two input numbers then it will concate those two values but won't add
+//we want to add the two input numbers for that we have to do some type checking
+//in javascript
+//if we write this code then in .ts file, there will be automatically given
+//type checking by typescript so no need of doing  manually we can check the error resolve at developement time
+
+
+// const button = document.getElementById("add");
+// const num1 = document.getElementById("num1");
+// const num2 = document.getElementById("num2");
+
+// function add(num1, num2) {
+//   if (typeof num1 === "number" && typeof num2 === "number") {
+//     return num1 + num2;
+//   }
+//   return +num1 + +num2;
+// }
+
+// button.addEventListener("click", () => {
+//   console.log("click...", add(num1.value, num2.value));
+// });
